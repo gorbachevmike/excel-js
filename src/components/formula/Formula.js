3a06f5a6db761730827e0df731240f1de9a5b0a7
Mikhail Gorbachev <gor.mihal@gmail.com>
2020-06-16 23:59:55 +0300

    Finish base table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,21 +1,40 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input'],
+      listeners: ['input', 'keydown'],
+      ...options,
     });
   }
   toHTML() {
     return `
       <div class="info">fx</div>
-      <div class="input" contenteditable spellcheck="false"></div>
+      <div class="input" data-type="input" contenteditable spellcheck="false"></div>
     `;
   }
+  init() {
+    super.init();
+    const $formula = this.$root.find('[data-type="input"]');
+    this.$on('table:select', ($cell) => {
+      $formula.text($cell.text());
+    });
+    this.$on('table:input', ($cell) => {
+      $formula.text($cell.text());
+    });
+  }
+
   onInput(event) {
-    console.log(this.$root);
-    console.log(event.target.textContent);
+    this.$emit('formula:input', $(event.target).text());
+  }
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab'];
+    if (keys.includes(event.key)) {
+      event.preventDefault();
+      this.$emit('formula:enter');
+    }
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,15 +4,31 @@ export class ExcelComponent extends DOMListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
+    this.prepare();
   }
+  // Настройка компонента до init
+  prepare() {}
   // Возвращает шаблон компонента
   toHTML() {
     return '';
   }
+  // Уведомляет слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+  // Инициализация компонента
   init() {
     this.initDOMListeners();
   }
+  // Удаляем компоненет
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach((unsub) => unsub());
   }
 }
