f13e04c7759495a489a77265cd2bf46238ac5a36
Mikhail Gorbachev <gor.mihal@gmail.com>
2020-06-17 18:59:35 +0300

    Finish redux and table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,9 @@ export class ExcelComponent extends DOMListener {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
+    this.subscribe = options.subscribe || [];
     this.unsubscribers = [];
+    this.store = options.store;
     this.prepare();
   }
   // Настройка компонента до init
@@ -22,6 +24,16 @@ export class ExcelComponent extends DOMListener {
     const unsub = this.emitter.subscribe(event, fn);
     this.unsubscribers.push(unsub);
   }
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+  // $subscribe(fn) {
+  //   this.storeSub = this.store.subscribe(fn);
+  // }
+  storeChanged() {}
+  isWatching(key) {
+    return this.subscribe.includes(key);
+  }
   // Инициализация компонента
   init() {
     this.initDOMListeners();
@@ -30,5 +42,6 @@ export class ExcelComponent extends DOMListener {
   destroy() {
     this.removeDOMListeners();
     this.unsubscribers.forEach((unsub) => unsub());
+    this.storeSub.unsubscribe();
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -12,7 +12,7 @@ class Dom {
     return this.$el.outerHTML.trim();
   }
   text(text) {
-    if (typeof text === 'string') {
+    if (typeof text !== 'undefined') {
       this.$el.textContent = text;
       return this;
     }
@@ -64,6 +64,12 @@ class Dom {
         });
     return this.$el;
   }
+  getStyles(styles = []) {
+    return styles.reduce((res, s) =>{
+      res[s] = this.$el.style[s];
+      return res;
+    }, {});
+  }
   id(parse) {
     if (parse) {
       const parsed = this.id().split(':');
@@ -85,6 +91,13 @@ class Dom {
   removeClass(className) {
     this.$el.classList.remove(className);
     return this;
+  }
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value);
+      return this;
+    }
+    return this.$el.getAttribute(name);
   }
 }
 export function $(selector) {
